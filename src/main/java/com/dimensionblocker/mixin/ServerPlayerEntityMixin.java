b258696fda0eb5dfc330ae4d8c2b0b8169ca25c7
MimStar <80539513+MimStar@users.noreply.github.com>
2025-08-22 17:26:28 +0200

    1.1.0 - Permissions update

--- src/main/java/com/dimensionblocker/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/dimensionblocker/mixin/ServerPlayerEntityMixin.java
@@ -4,6 +4,7 @@ import com.dimensionblocker.ConfigData;
 import com.dimensionblocker.PlayerData;
 import com.dimensionblocker.Dimensionblocker;
 import com.dimensionblocker.StateSaverAndLoader;
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -17,15 +18,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.concurrent.ExecutionException;
+
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
 
-    @Inject(method = "teleportTo", at = @At("HEAD"), cancellable = true)
-    private void onTeleport(TeleportTarget teleportTarget, CallbackInfoReturnable<Entity> cir){
+    @Inject(method = "teleportTo(Lnet/minecraft/world/TeleportTarget;)Lnet/minecraft/server/network/ServerPlayerEntity;", at = @At("HEAD"), cancellable = true)
+    private void onTeleport(TeleportTarget teleportTarget, CallbackInfoReturnable<Entity> cir) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
         ConfigData configData = StateSaverAndLoader.getConfigState(teleportTarget.world().getServer());
         PlayerData playerData = StateSaverAndLoader.getPlayerState(player);
-        if (configData.getDimensions().contains(teleportTarget.world().getRegistryKey().getValue().toString())){
+        String permission = "dimensionblocker.allow." + teleportTarget.world().getRegistryKey().getValue().toString().replace(":", ".");
+        if (configData.getDimensions().contains(teleportTarget.world().getRegistryKey().getValue().toString()) && !Permissions.check(player.getUuid(),permission).join()){
             player.sendMessage(Text.literal(Dimensionblocker.getTranslation(playerData.getLanguage(),"dimension_cancel")).formatted(Formatting.RED), false);
             cir.setReturnValue(null);
             cir.cancel();
